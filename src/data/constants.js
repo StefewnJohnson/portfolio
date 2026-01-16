873927e5b79bef3b0e2edbed07334c6bd52f310d
Stefewn Johnson <81782278+StefewnJohnson@users.noreply.github.com>
2026-01-16 16:10:34 -0500

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,362 @@
+export const Bio = {
+    name: "Stefewn Johnson",
+    roles: [
+      "Software Engineer",
+      "Full Stack Developer",
+      "Machine Learning Engineer",
+      "Data Engineer",
+      "AI Engineer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/StefewnJohnson",
+    resume:
+      "https://drive.google.com/file/d/1dxoNAgwGSv8gSpxVOBRiWPtL5zVpfMmz/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/stefewn-johnson/",
+    github: "https://github.com/StefewnJohnson",
+    //twitter: "https://twitter.com/RishavChanda",
+    //insta: "https://www.instagram.com/rishav_chanda/",
+    //facebook: "https://www.facebook.com/rishav.chanda.165/",
+  };
+  
+  export const skills = [
+    {
+      title: "Languages",
+      skills: [
+        {
+          name: "Java",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "C",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/1853px-C_Programming_Language.svg.png",
+        },
+        {
+          name: "C#",
+          image:
+            "https://cdn.worldvectorlogo.com/logos/c--4.svg",
+        },
+        {
+          name: "C++",
+          image:
+            "https://www.vikingsoftware.com/wp-content/uploads/2024/02/C-2.png",
+        },
+        {
+          name: "SQL",
+          image:
+            "https://db.cs.uni-tuebingen.de/teaching/ws2223/sql-is-a-programming-language/logo.svg",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "PHP",
+          image:
+            "https://cdn.iconscout.com/icon/free/png-256/free-php-2038871-1720084.png?f=webp&w=256",
+        },
+        
+        
+        
+      ],
+    },
+    {
+      title: "Frameworks & Databases",
+      skills: [
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+        {
+          name: "PostgreSQL",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/993px-Postgresql_elephant.svg.png",
+        },
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "Next Js",
+          image:
+            "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+        },
+        {
+          name: "Angular",
+          image:
+            "https://camo.githubusercontent.com/8886130b3d8aba95dbdd7c4f9a41029606424cc06d1873c1ced87dd55a222fef/68747470733a2f2f616e67756c61722e696f2f6173736574732f696d616765732f6c6f676f732f616e67756c61722f616e67756c61722e737667",
+        },
+        {
+          name: "Spring",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Spring_Boot.svg/640px-Spring_Boot.svg.png",
+        },
+      ],
+    },
+    {
+      title: "Tools",
+      skills: [
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "VS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/2/2c/Visual_Studio_Icon_2022.svg",
+        },
+        {
+          name: "Google Vertex AI",
+          image:
+            "https://thirdeyedata.ai/wp-content/uploads/2021/07/VertexAI-512-color.png",
+        },
+        {
+          name: "Google Colab",
+          image:
+            "https://avatars.githubusercontent.com/u/33467679?s=280&v=4",
+        },
+        {
+          name: "GPT-2",
+          image:
+            "https://res.cloudinary.com/apideck/image/upload/v1615960059/icons/gpt-2.png",
+        },
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+      ],
+    },
+    {
+      title: "Platforms",
+      skills: [ 
+        {
+          name: "Linux",
+          image: "https://pngimg.com/d/linux_PNG1.png",
+        },
+        {
+          name: "Jupyter Notebook",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Jupyter_logo.svg/1200px-Jupyter_logo.svg.png",
+        },
+        {
+          name: "Google Cloud",
+          image:
+            "https://static-00.iconduck.com/assets.00/google-cloud-icon-2048x1646-7admxejz.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+        {
+          name: "RISC-V",
+          image:
+            "https://avatars.githubusercontent.com/u/81319613?s=280&v=4",
+        },
+        {
+          name: "Adobe Software",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0gSSPIHfPrsKzUBg7qoY1nmttPvvC1GtaNQ&s",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://www.theglobeandmail.com/resizer/hMhEz1i79KAmXvteNAmF1Es34jc=/arc-anglerfish-tgam-prod-tgam/public/6KE3L65AFNDBJAMVBXZY6ZQGCE.JPG",
+      role: "Programmer/Analyst Co-op",
+      company: "TTC, Toronto, ON",
+      date: "May 2025 - PRESENT",
+      desc: "Built a maintenance system and database in Microsoft Access, enabling admin users to issue maintenance notices. Rebuilt TTC’s old Microsoft Access forms and reports using HTML, CSS, and JavaScript for better functionality and UI. Developed an employee database in Microsoft Access to streamline the process of updating TTC employee information, using SQL and VBA to reduce the task to a two-button operation. Engineered a calendar application using HTML, CSS, and JavaScript, displaying historical and current train service data for each day, peak, and train line for TTC staff",
+      skills: [
+        "SQL",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Visual Basic for Applications (VBA)",
+        "Microsoft Access",
+        "Ultra Edit",
+      ],
+      //doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+    },
+    {
+      id: 1,
+      img: "https://pbs.twimg.com/profile_images/1306241109924950018/4n0WOra7_400x400.jpg",
+      role: "Head Soccer Coach",
+      company: "Thornhill Soccer Club, Thornhill, ON",
+      date: "May 2023 - PRESENT",
+      desc: "Meticulously craft session plans for training sessions and effectively organize teams to secure victories. Take charge of cleaning and maintaining the sports area, ensuring a safe and welcoming environment for all. Facilitate the development of players' technical skills and soccer knowledge through focused training and mentorship. Cultivate strong leadership, control, and planning skills essential for driving team performance and success.",
+      skills: [
+        "Communication",
+        "Leadership",
+        "Teamwork",
+        "Organization Skills",
+        "Time Management",
+        "Coaching",
+      ],
+      //doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+    },
+    {
+      id: 2,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQiXAsA6tjbgJ30S0yxSapjiUCgheDWmVAEw&s",
+      role: "Sports Attendant",
+      company: "City of Vaughan",
+      date: "Apr 2022 - June 2025",
+      desc: "Teach children various sports, prioritizing their enjoyment and safety in a positive and supportive environment. Take responsibility for cleanliness and maintenance of sports area, ensuring safe and hygienic space for activities. Hone strong verbal communication and interpersonal skills, effectively engaging with children and colleagues. Develop solid organizational skills to manage tasks efficiently. Cultivate collaborative skills by working closely with teams, contributing to a cohesive and effective team dynamic.",
+      skills: [
+        "Communication",
+        "Interpersonal Skills",
+        "Teaching",
+        "Safety Awareness",
+        "Risk Management",
+      ],
+    },
+    {
+      id: 3,
+      img: "https://www.theglobeandmail.com/resizer/hMhEz1i79KAmXvteNAmF1Es34jc=/arc-anglerfish-tgam-prod-tgam/public/6KE3L65AFNDBJAMVBXZY6ZQGCE.JPG",
+      role: "Graphic Designer",
+      company: "TTC, Toronto, ON",
+      date: "May 2023 - August 2023",
+      desc: "Managed and cleaned up images using Photoshop, and then published them on their website. Used Microsoft Outlook to email clients if they are interested in ordering ride guides. Utilized Microsoft PowerPoint to make a presentation on Black History Month and Black Indigenous Month. Leveraged Microsoft Excel to manage client information about ride guides.",
+      skills: [
+        "Software Design",
+        "User Experience (UX)",
+        "Online Graphics",
+        "Image Design",
+        "Adobe Photoshop",
+        "WordPress",
+        "Microsoft Office",
+      ],
+      //doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8rHrGdb-8ROOm1Y0iaZQ7fczblKh4mqab_Q&s",
+      school: "York University, North York, ON",
+      date: "Sep 2022 - May 2026",
+      grade: "7.0/9.0 GPA",
+      desc: "Activities and societies: I am currently enrolled in the prestigious Bergeron Entrepreneurs in Science and Technology certification program. This unique program combines courses from the esteemed Lassonde School of Engineering, Schulich School of Business, and Osgoode Hall Law School. It offers a comprehensive blend of technical expertise, business acumen, and legal understanding, preparing me to excel in the dynamic intersection of science, technology, and entrepreneurship.",
+      degree: "Specialized Honours Bachelor of Science, Computer Science",
+    },
+    {
+      id: 1,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4mbIzvW76PTadcpcO-RLCC4e-VwcShkfQ7Q&s",
+      school: "Cardinal Carter High School, Aurora, ON",
+      date: "Sep 2018 - Jun 2022",
+      grade: "95.0%",
+      desc: "Activities and societies: Deca Club | Soccer Club | Stock Club",
+      degree: "High School Diploma",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Emotion Detector",
+      date: "Apr 2024 - Jun 2024",
+      description:
+        "Leveraged Google Cloud platform to execute Python code and conduct required tasks, including model training and fine-tuning(AI Programming to react a certain way for each given information). Employed Google Vertex AI and PowerBI alongside Python and GPT-2 for seamless execution and analysis of model training and dataset exploration tasks. This AI model is able to detect emotion from a conversation between two people such as detecting if someone is happy, sad, or angry. It is able to detect it through the words and emotions used in the conversation.",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Google_Colaboratory_SVG_Logo.svg/1280px-Google_Colaboratory_SVG_Logo.svg.png",
+      tags: ["Python", "Google Cloud", "Power BI", "GBT-2", "Google Vertex AI", "Google Colab"],
+      category: "machine learning",
+      webapp: "https://github.com/StefewnJohnson/Emotion-Detector",
+    },
+    {
+      id: 1,
+      title: "Edify",
+      date: "Oct 2023 - Jan 2024",
+      description:
+        "Spearheaded the creation of Edify, an innovative AI-driven online learning platform aligned with the UN Sustainability Goal for Quality Education. Designed to cater to individual learning styles while ensuring privacy and safety for children, Edify leverages predictive analytics and collaboration tools for a personalized educational experience. Led a diverse team of experts in web design, graphic design, game development, web development, AI, and machine learning to develop an inclusive and effective learning environment.",
+      image:
+        "https://raw.githubusercontent.com/StefewnJohnson/stefewnjohnson.github.io/main/src/images/Screen%20Shot%202024-07-24%20at%209.56.26%20AM.png",
+        //"https://edifylearning.github.io/EdifyWebsite/assets/img/Logo%20Edify%20(11)%20(1).png",
+      tags: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Visual Studio Code"
+      ],
+      category: "website",
+      webapp: "https://edifylearning.github.io/EdifyWebsite/",
+    },
+    {
+      id: 2,
+      title: "Awesome Platformer",
+      date: "Jun 2022 - Aug 2022",
+      description:
+        "Developed a dynamic Unity-based game using C#, featuring a diverse range of monsters with unique abilities and attack patterns to enhance player engagement. Implemented varied combat strategies with multiple weapons and skills, culminating in a challenging final boss with special attacks and strong defenses. Designed immersive levels with traps, puzzles, and enemy hordes, and ensured a captivating experience through high-quality graphics and sound effects. Created a user-friendly main menu for starting new games, loading saved progress, and adjusting settings, ensuring a compelling and immersive gameplay experience.",
+      image:
+        "https://raw.githubusercontent.com/StefewnJohnson/Awesome-Platformer/main/UI/BG.png",
+      tags: ["C#", "Unity", "Visual Studio", "MonoDevelop"],
+      category: "game",
+      webapp: "https://github.com/StefewnJohnson/Awesome-Platformer",
+    },
+    {
+      id: 3,
+      title: "The Village (Java Game)",
+      date: "May 2022 - Jun 2022",
+      description:
+        "Developed a text-based game using Java on Replit, utilizing the platform’s collaborative coding features. Focused on narrative and player choices, with customization options for character names and difficulty levels. Designed various enemies and bosses with unique attributes and attack patterns, and implemented combat mechanics through text commands for strategic gameplay. Incorporated an XP system for leveling up and enhancing abilities, and crafted a compelling story that evolves based on player decisions. Integrated a range of weapons with distinct effects, and included multiple difficulty levels to cater to diverse player preferences. Ensured straightforward navigation through a text-based interface and enhanced replayability with multiple difficulty settings and story paths.",
+      image:
+        "https://149455152.v2.pressablecdn.com/wp-content/uploads/2021/05/REvillagegeekdadfull.png",
+      tags: ["Java", "Replit"],
+      category: "program",
+      webapp: "https://github.com/StefewnJohnson/The-Village-Game",
+    },
+    {
+      id: 4,
+      title: "Tic-Tac-Toe",
+      date: "May 2021 - Jun 2021",
+      description:
+        "Developed a Tic-Tac-Toe game using Java and NetBeans, featuring a responsive design and user-friendly interface. Utilized 3-D arrays to manage the game board and track progress, ensuring accurate detection of winning combinations and smooth turn-based gameplay. Added a game history feature to allow players to review previous moves and outcomes.",
+      image:
+        "https://cdn-icons-png.flaticon.com/512/10199/10199746.png",
+      tags: ["Java", "NetBeans"],
+      category: "program",
+      webapp: "https://github.com/StefewnJohnson/Tic-Tac-Toe",
+    },
+  ];
+  
+  export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+  ];
